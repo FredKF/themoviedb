1f2deecf34f4f9dad74e576647c6e702703cbb65
FredKF <fedetkd86@hotmail.com>
2021-05-09 13:10:08 +0200

    default components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MovieItemComponent,
+    MovieDetailComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule
