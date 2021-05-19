72f3dbcdf24385eeac99f984166959de7614c531
FredKF <fedetkd86@hotmail.com>
2021-05-19 21:33:03 +0200

    Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/shared/header/header.component';
 import { MovieItemComponent } from './components/movie-item/movie-item.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MovieListComponent } from './components/movie-list/movie-list.component
     MovieListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
