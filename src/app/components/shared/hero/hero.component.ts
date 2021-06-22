ad5a9f8bbaf2ebcfe8b2bd412bee4a2e69c085e4
Fede Copperi <federico.guillermo@col.vueling.com>
2021-06-22 14:42:49 +0200

    styling pages

--- src/app/components/shared/hero/hero.component.ts
+++ src/app/components/shared/hero/hero.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
+  images: string[] = [];
+  actualImage: string;
+  changeBackgroundCounter = 0;
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.images = ["../../../../assets/images/raiders.jpg",
+    "../../../../assets/images/mandalorian.jpg",
+    "../../../../assets/images/kong.jpg",
+    "../../../../assets/images/scarface.jpg",
+    "../../../../assets/images/starwars.jpg",
+    "../../../../assets/images/terminator.jpg"];
 
-}
+
+      console.log(this.images[0]);
+
+
+      this.actualImage = this.images[0];
+      setInterval(() => {
+        this.changeBackgroundCounter++;
+        if (this.changeBackgroundCounter > this.images.length - 1) {
+          this.changeBackgroundCounter = 0;
+        }
+        this.actualImage = this.images[this.changeBackgroundCounter];
+      }, 5000);
+    }
+
+  }
